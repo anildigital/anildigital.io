24f7817bbbc4fac0a533d16c8e7ae1ac2b1ba125
Anil Wadghule <anildigital@gmail.com>
2015-02-16 21:43:21 +0530

    Wrap om code in main

--- src/cljs/anildigital_io/core.cljs
+++ src/cljs/anildigital_io/core.cljs
@@ -16,21 +16,22 @@
         ))
   )
 
-(om/root 
- (fn [data owner]
-   (reify
-     om/IInitState
-     (init-state [_]
-       (swap! app-state assoc :list [{:name "loading..."}])
-       )
-     om/IDidMount
-     (did-mount [_]
-       (fetch-feed)
-       (js/setInterval (fn [] (fetch-feed)) 10000)
-       ) 
-     om/IRender
-     (render [_]
-       (apply dom/ul #js {:className "music_list"}
-              (map (fn [text] (dom/li nil (str (get-in text [:name]) " - " (get-in text [:artist :#text]) ) )) (:list data))))))
- app-state
- {:target (. js/document (getElementById "app"))})
+(defn main []
+  (om/root 
+   (fn [data owner]
+     (reify
+       om/IInitState
+       (init-state [_]
+         (swap! app-state assoc :list [{:name "loading..."}])
+         )
+       om/IDidMount
+       (did-mount [_]
+         (fetch-feed)
+         (js/setInterval (fn [] (fetch-feed)) 10000)
+         ) 
+       om/IRender
+       (render [_]
+         (apply dom/ul #js {:className "music_list"}
+                (map (fn [text] (dom/li nil (str (get-in text [:name]) " - " (get-in text [:artist :#text]) ) )) (:list data))))))
+   app-state
+   {:target (. js/document (getElementById "app"))}))
