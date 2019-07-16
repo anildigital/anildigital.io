07458726e51d53e6f8cc6ff3a998f3b2257aa0a8
Anil Wadghule <anildigital@gmail.com>
2019-07-16 20:15:18 +0530

    Try ssl changes for Heroku deployment

--- config/prod.exs
+++ config/prod.exs
@@ -10,8 +10,10 @@ use Mix.Config
 # which you should run after static files are built and
 # before starting your production server.
 config :anildigital_io, AnildigitalIoWeb.Endpoint,
+  http: [:inet6, port: System.get_env("PORT") || 4000],
   url: [host: "anildigital.io", port: 443, scheme: "https"],
-  cache_static_manifest: "priv/static/cache_manifest.json"
+  cache_static_manifest: "priv/static/cache_manifest.json",
+  force_ssl: [rewrite_on: [:x_forwarded_proto]]
 
 # Do not print debug messages in production
 config :logger, level: :info
